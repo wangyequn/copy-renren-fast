1df89c24ffbc26a0bc558c88c79daee38eff13ae
yequn <yequnat@163.com>
2019-02-11 17:39:48 +0800

    初始化项目

---
+++ api/renren-fast-copy-api/src/main/java/cn/wangyequn/renrenfastcopyapi/RenrenFastCopyApiApplication.java
@@ -0,0 +1,14 @@
+package cn.wangyequn.renrenfastcopyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenrenFastCopyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenrenFastCopyApiApplication.class, args);
+	}
+
+}
+
---
+++ api/renren-fast-copy-api/src/main/java/cn/wangyequn/renrenfastcopyapi/controller/DemoControlloer.java
@@ -0,0 +1,11 @@
+package cn.wangyequn.renrenfastcopyapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoControlloer {
+	
+	public void init() {
+		
+	}
+}
