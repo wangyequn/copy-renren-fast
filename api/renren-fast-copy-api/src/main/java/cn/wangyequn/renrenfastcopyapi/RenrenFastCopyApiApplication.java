032a9b5953e74879c6aacb5729a5d342c8b0eaf9
yequn <yequnat@163.com>
2019-02-12 17:48:57 +0800

    尝试添加mybatis plus

--- api/renren-fast-copy-api/src/main/java/cn/wangyequn/renrenfastcopyapi/RenrenFastCopyApiApplication.java
+++ api/renren-fast-copy-api/src/main/java/cn/wangyequn/renrenfastcopyapi/RenrenFastCopyApiApplication.java
@@ -1,9 +1,11 @@
 package cn.wangyequn.renrenfastcopyapi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.baomidou.mybatisplus.samples.quickstart.mapper")
 public class RenrenFastCopyApiApplication {
 
 	public static void main(String[] args) {
--- api/renren-fast-copy-api/src/main/java/cn/wangyequn/renrenfastcopyapi/controller/DemoControlloer.java
+++ api/renren-fast-copy-api/src/main/java/cn/wangyequn/renrenfastcopyapi/controller/DemoControlloer.java
@@ -1,11 +1,21 @@
 package cn.wangyequn.renrenfastcopyapi.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoControlloer {
 	
-	public void init() {
-		
+	@RequestMapping(value="/test",method=RequestMethod.GET)
+	public Map<String,String> init() {
+		Map<String,String> map = new HashMap<>();
+		map.put("text1", "text1");
+		map.put("text2", "text2");
+		map.put("text3", "text4");
+		return map;
 	}
 }
